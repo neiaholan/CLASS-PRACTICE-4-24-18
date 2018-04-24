d31de9dfa5243a0c056fec57da74fc21ae2d115b
Hunter <NOHunter.2014@MyMail.jcsu.edu>
2018-04-24 13:38:14 -0400

    first commit

---
+++ AnotherOne.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class AnotherOne {
+	
+	public static void main (String[] args){
+		Scanner can= new Scanner(System.in);
+		int [] nums= {7,77,777}
+		int largest= nums[0];
+			for (int i=0; i<3;i++){
+				largest=nums[i];
+				
+			}
+			
+	}
+			System.out.println(largest);
+			int smallest=nums[0];
+			for (int i=0; i<3; i++){
+				
+				if (nums[i]<smallest) {
+					smallest=nums[i];
+			
+	}
+	}
+{
+	System.out.println(smallest);
+}
+
---
+++ Project.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Project {
+public static void main(String [] args){
+	
+	double length;
+	int NumberOfPizzas;
+}
+
+{	System.out.println("How many Pizzas do you want?");
+	String [] NumberOfPizzas; x=new String [scan.nextInt()];
+	for (int i=0; i < x length 1++){
+		System.out.println("Enter type"+(i+1));
+		x[i]=scan.next();
+	}
+	
+	System.out.println("What Pizza do you want?");
+	String y=scan.next();
+	
+	String a = "No match found";
+	for (int z=0 ; z < x length ;z++);
+	if (y.equals(x[z])){
+		a="match found";
+	}
+}
+System.out.print(a);
+}
+	}
+}
+}
